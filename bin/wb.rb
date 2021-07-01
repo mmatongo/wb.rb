beaef9537bac67e1b1164b42a6a3e9badc44366c
Daniel M. Matongo <me@danielm.pw>
2021-07-01 21:31:15 +0200

    wb: expanded class to method & added more options

--- bin/wb.rb
+++ bin/wb.rb
@@ -1,30 +1,37 @@
 class WB
-    
-    while true
-        # get user input for what they want to init
-        prompt = "> "
 
-        puts "\nType ':q' to quit and ':h' for help"
-        print "#{prompt}"
-        input = gets.chomp
+    def self.run
+        while true
+            # get user input for what they want to init
+            prompt = "> "
 
-        # exit the program if it matches key words.
-        break if input == ":q" || input == "quit" || input == "exit"
+            puts "\nType ':q' to quit and ':h' for help"
+            print "#{prompt}"
+            input, projectName = ARGV  #gets.chomp will be used but not now, I need to test ARGV
 
-        case input
-        when ":h"
-            WB.help
-        when "go"
-            WB.golang
-        when "rust"
-            WB.c
-        else
-            puts "\n Invalid command, type ':h' to see available commands."
+            # exit the program if it matches key words.
+            break if input == ":q" || input == "quit" || input == "exit"
+
+            # get user input for the 
+            case input
+            when ":h"
+                WB.help
+            when "c"
+                WB.c
+            when "go"
+                WB.golang
+            when "sh"
+                WB.sh
+            when "rust"
+                WB.rust
+            else
+                puts "\n Invalid command, type ':h' to see available commands."
+            end
         end
     end
 end
 
-def help
+def self.help
     print "Help"
     print ":h - This help menu."
     print ":q - Quit the application."
