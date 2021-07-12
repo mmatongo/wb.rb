dc31c2d5e3a66dfe5dd07fba2b4baf007eee8456
Matongo Mulindi <mmatongo_@hotmail.com>
2021-07-12 12:48:27 +0200

    minor touch up

--- lib/wb.rb
+++ lib/wb.rb
@@ -10,7 +10,7 @@ class WB
 
       puts "\nType ':q' to quit and ':h' for help"
       print prompt.to_s
-      input = $stdin.gets.chomp!.to_s
+      input = $stdin.gets.chomp.to_s
 
       # exit the program if it matches key words.
       break if [':q', 'quit', 'exit'].include?(input)
